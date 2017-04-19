84b5f1dff72dc2e1bf9cc5422ea2e18a61ebbd61
songchao <1014126299@qq.com>
2017-04-19 16:06:10 +0800

    init

--- src/MD5Hasher.php
+++ src/MD5Hasher.php
@@ -15,6 +15,7 @@ class MD5Hasher
      */
     public function make($value, array $options=[]) {
         $salt = isset($options['salt'])?$options['salt']:'';
+
         return hash('md5',$value.$salt);
     }
 
